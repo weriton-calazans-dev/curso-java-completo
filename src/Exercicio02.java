2aa6fb9c5b5fd814d002b5b861dad254a05c44a4
Weriton calazans <weritoncalazans.dev@gmail.com>
2026-06-18 16:03:51 -0300

    Exercício 2 de média de notas concluído

---
+++ src/Exercicio02.java
@@ -0,0 +1,13 @@
+public class Exercicio02 {
+    public static void main(String[] args){
+        String nome = "weriton";
+        double nota1 = 8.5;
+        double nota2 = 7.0;
+        double nota3 = 9.2;
+        double media = (nota1 + nota2 + nota3) / 3.0;
+
+        System.out.printf("A media do aluno %s é: %.1f%n ", nome, media);
+
+    }
+
+}

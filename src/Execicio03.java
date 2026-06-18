40f197d11ee37c6279b2ad99db9ea976e8563970
Weriton calazans <weritoncalazans.dev@gmail.com>
2026-06-18 18:38:23 -0300

    Exercício 4 de soma interativa com Scanner concluído

--- src/Execicio03.java
+++ src/Execicio03.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 public class Execicio03 {
     public static void main(String[] args) {
         double celsius = 30.0;
         double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
 
         System.out.printf("A temperatura de %.1f C equivale a %.1f F. %n", celsius, fahrenheit);
+
     }
 
 }
---
+++ src/Execicio04.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Execicio04 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número para a soma:");
+        int numero1 = sc.nextInt();
+        System.out.println("Digite mais um número para a soma");
+        int numero2 = sc.nextInt();
+
+        int resultado = numero1 + numero2;
+        System.out.println("A soma dos números é: " + resultado);
+
+        sc.close();
+    }
+}
